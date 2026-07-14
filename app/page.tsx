c12ee6fa19da42f7a6d8c5a93c37f7e69192a39e
nicolasprothero <nprothero0917@gmail.com>
2026-07-13 21:12:58 -0400

    basic init

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
-import Image from "next/image";
-import { Button } from "hase-ui";
+import { Flex, Div, Header, Button } from "hase-ui";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Button>Hey</Button>
-      </main>
-    </div>
+    <Div>
+      <Header>NIC</Header>
+
+      <Flex
+        direction="row"
+        justify="center"
+        align="center"
+        gap="12px"
+        paddingX="20px"
+        marginY="20px"
+      >
+        <Button>Click Me</Button>
+      </Flex>
+    </Div>
   );
 }
